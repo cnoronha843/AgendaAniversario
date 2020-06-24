cf342b321413d99fc1e9d58966004878dd5e8702
Carlos Andre Noronha <carlosanoronha@hotmail.com>
2020-06-23 22:44:45 -0300

    Agenda de aniversario

---
+++ AgendaAniversario/Controllers/PessoaController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgendaAniversario.Models;
+using AgendaAniversario.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaAniversario.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private PessoaRepository PessoaRepository { get; set; }
+
+        public PessoaController (PessoaRepository pessoaRepository)
+        {
+            this.PessoaRepository = pessoaRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = this.PessoaRepository.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+      
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pessoa  pessoa)
+        {
+            try
+            {
+                this.PessoaRepository.Save(pessoa);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            var model = this.PessoaRepository.GetPessoaById(id);
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Pessoa model)
+        {
+            try
+            {
+                model.Id = id;
+                this.PessoaRepository.Update(model);
+                return RedirectToAction(nameof(Index));
+            }
+
+            catch
+            {
+                return View();
+            }
+            
+        }
+
+        public ActionResult Search ([FromQuery] string query )
+        {
+            var model = this.PessoaRepository.Search(query);
+
+            return View("Index", model);
+
+        }
+        public IActionResult Delete (int id)
+        {
+
+            var model = this.PessoaRepository.GetPessoaById(id);
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete (int id, Pessoa model)
+        {
+            try
+            {
+                model.Id = id;
+                this.PessoaRepository.Delete(model);
+                return RedirectToAction(nameof(Index));
+            }
+
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = this.PessoaRepository.GetPessoaById(id);
+            return View(model);
+            
+        }
+
+
+
+    }
+}
---
+++ AgendaAniversario/Repository/PessoaRepository.cs
@@ -0,0 +1,227 @@
+﻿using AgendaAniversario.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaAniversario.Repository
+{
+    public class PessoaRepository
+    {
+
+        private string connectionString { get; set; }
+
+        public PessoaRepository (IConfiguration configuration) 
+        {
+            this.connectionString = configuration.GetConnectionString("Gerenciamento");
+        }
+
+        public void Save (Pessoa pessoa)
+        {
+            using(SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string sql = " INSERT INTO PESSOA (NOME, SOBRENOME, DATANASCIMENTO) VALUES (@P1, @P2, @P3)";
+
+
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.Parameters.AddWithValue("P1", pessoa.Nome);
+                command.Parameters.AddWithValue("P2", pessoa.Sobrenome);
+                command.Parameters.AddWithValue("P3", pessoa.DataNascimento);
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();        
+
+            }
+
+        }
+
+        public void Update (Pessoa pessoa)
+        {
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                
+
+                string sql = @"UPDATE PESSOA 
+                               SET NOME = @P1,
+                               SOBRENOME = @P2,
+                               DATANASCIMENTO = @P3
+                               WHERE ID = @P4;
+
+                ";
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.Parameters.AddWithValue("P1", pessoa.Nome);
+                command.Parameters.AddWithValue("P2", pessoa.Sobrenome);
+                command.Parameters.AddWithValue("P3", pessoa.DataNascimento);
+                command.Parameters.AddWithValue("P4", pessoa.Id);
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+
+        }
+        public void Delete (Pessoa pessoa)
+        {
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+
+
+                string sql = @"DELETE FROM PESSOA
+                               WHERE ID = @P1;
+
+                ";
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.Parameters.AddWithValue("P1", pessoa.Id);                
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+
+        }
+
+        public List<Pessoa> GetAll ()
+        {
+
+            List<Pessoa> result = new List<Pessoa>();
+
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+
+
+                string sql = @"SELECT ID, NOME, SOBRENOME, DATANASCIMENTO
+                               FROM PESSOA ORDER BY DATANASCIMENTO ASC
+                               
+
+                ";
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+                SqlDataReader dr = command.ExecuteReader();
+
+                while(dr.Read())
+                {
+                    result.Add(new Pessoa()
+                    {
+                        Id = dr.GetInt32("ID"),
+                        Nome = dr.GetString("NOME"),
+                        Sobrenome = dr.GetString("SOBRENOME"),
+                        DataNascimento = dr.GetDateTime("DATANASCIMENTO")
+                    });
+                }
+
+
+                
+                connection.Close();
+
+            }
+            return result;
+
+        }
+
+        public Pessoa GetPessoaById(int id)
+        {
+
+            Pessoa result = null;
+
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+
+
+                string sql = @"SELECT ID, NOME, SOBRENOME, DATANASCIMENTO
+                               FROM PESSOA
+                               WHERE ID = @P1
+                               
+
+                ";
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.Parameters.AddWithValue("P1", id);
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+
+                SqlDataReader dr = command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    result = new Pessoa()
+                    {
+                        Id = dr.GetInt32("ID"),
+                        Nome = dr.GetString("NOME"),
+                        Sobrenome = dr.GetString("SOBRENOME"),
+                        DataNascimento = dr.GetDateTime("DATANASCIMENTO")
+                    };
+                }
+               
+                connection.Close();
+
+            }
+
+            return result;
+        
+
+        }
+
+        public List<Pessoa> Search(string query)
+        {
+
+            List<Pessoa> result = new List<Pessoa>();
+
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+
+
+                string sql = @"SELECT ID, NOME, SOBRENOME, DATANASCIMENTO
+                               FROM PESSOA
+                               WHERE (NOME LIKE '%" + query + "%' OR SOBRENOME LIKE '%" + query + "%')";
+                               //ORDER BY DATANASCIMENTO ASC
+                               
+
+                
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                //command.Parameters.AddWithValue("P1", query);
+               // command.Parameters.AddWithValue("P2", query);
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+
+                SqlDataReader dr = command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    result.Add(new Pessoa()
+                    {
+                        Id = dr.GetInt32("ID"),
+                        Nome = dr.GetString("NOME"),
+                        Sobrenome = dr.GetString("SOBRENOME"),
+                        DataNascimento = dr.GetDateTime("DATANASCIMENTO")
+                    });
+                }
+
+                connection.Close();
+
+            }
+
+            return result;
+
+
+        }
+
+    }
+}
